780a0b8e063546cfe5872baca7f7b958f8c1658e
YunL97 <leeys9932@naver.com>
2023-10-11 14:52:22 +0900

    빌드별 baseurl설정 및 env 설정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
   // env 내용 추가
     env: {
-    BASE_URL: process.env.BASE_URL,
+    BASE_URL: process.env.NEXT_PUBLIC_URL,
     },
     ...(process.env.NEXT_PUBLIC_NODE_ENV === "prod" && {
         compiler: {
