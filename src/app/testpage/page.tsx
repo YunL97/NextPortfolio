cbef7d34646f8248b9d3fc39723a0da056483974
YunL97 <leeys9932@naver.com>
2023-10-11 15:14:00 +0900

    eslint, prettier 셋팅

--- src/app/testpage/page.tsx
+++ src/app/testpage/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 
 export default function TestComponent() {
-    const fullScreenDivStyle = {
-        width: '100vw', // 100% 화면 너비
-        height: '100vh', // 100% 화면 높이
-        backgroundColor: 'lightgray', // 배경색을 변경하거나 스타일을 원하는 대로 수정하세요
-    };
-    console.log('test'  + process.env.BASE_URL)
-    console.log(process.env.NEXT_PUBLIC_NODE_ENV)
-    return (
-        <main style={fullScreenDivStyle}>asdasd</main>
-        )
-    }  
+  const fullScreenDivStyle = {
+    width: '100vw', // 100% 화면 너비
+    height: '100vh', // 100% 화면 높이
+    backgroundColor: 'lightgray' // 배경색을 변경하거나 스타일을 원하는 대로 수정하세요
+  }
+  console.log('test' + process.env.BASE_URL)
+  console.log(process.env.NEXT_PUBLIC_NODE_ENV)
+  return <main style={fullScreenDivStyle}>asdasd</main>
+}
