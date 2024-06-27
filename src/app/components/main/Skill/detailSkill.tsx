0705ebb95c2bc54fd0ff89698866bece1e2386c2
YunL97 <leeys9932@naver.com>
2024-06-27 23:14:21 +0800

    이미지 링크 변경

--- src/app/components/main/Skill/detailSkill.tsx
+++ src/app/components/main/Skill/detailSkill.tsx
@@ -1,42 +1,53 @@
 const FrontSkill = () => {
   return (
-    <div className=" overflow-x-auto p-28  w-full">
+    <div className="overflow-x-auto p-28 w-full">
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Frontend</h2>
-        <div className=" flex flex-wrap">
-          <img src="/HTML.svg" alt="HTML5" className="h-8" />
-          <img src="/css.svg" alt="CSS3" className="h-8" />
-          <img src="/javascript.svg" alt="JavaScript" className="h-8" />
-          <img src="/typescript.svg" alt="JavaScript" className="h-8" />
-          <img src="/vue.svg" alt="JavaScript" className="h-8" />
-          <img src="/react.svg" alt="JavaScript" className="h-8" />
-          <img src="/nextjs-dark.svg" alt="JavaScript" className="h-8" />
+        <div className="flex flex-wrap">
+          <img src="/HTML.svg" alt="HTML5" className="h-8 mr-2 mb-2" />
+          <img src="/CSS.svg" alt="CSS3" className="h-8 mr-2 mb-2" />
+          <img
+            src="/JavaScript.svg"
+            alt="JavaScript"
+            className="h-8 mr-2 mb-2"
+          />
+          <img
+            src="/TypeScript.svg"
+            alt="TypeScript"
+            className="h-8 mr-2 mb-2"
+          />
+          <img src="/Vue.svg" alt="Vue.js" className="h-8 mr-2 mb-2" />
+          <img src="/React.svg" alt="React" className="h-8 mr-2 mb-2" />
+          <img src="/NextJS-Dark.svg" alt="Next.js" className="h-8 mr-2 mb-2" />
         </div>
       </div>
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Backend</h2>
-        <div className=" flex flex-wrap">
-          <img src="/nodejs.svg" alt="Python" className="h-8" />
-          <img src="/python-dark.svg" alt="Python" className="h-8" />
-          <img src="/fastapi.svg" alt="Django" className="h-8" />
+        <div className="flex flex-wrap">
+          <img src="/NodeJS.svg" alt="Node.js" className="h-8 mr-2 mb-2" />
+          <img src="/Python-Dark.svg" alt="Python" className="h-8 mr-2 mb-2" />
+          <img src="/FastAPI.svg" alt="FastAPI" className="h-8 mr-2 mb-2" />
         </div>
       </div>
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Mobile App</h2>
-        <div className=" flex flex-wrap">
-          <img src="/dart-light.svg" alt="JavaScript" className="h-8" />
-          <img src="/flutter-light.svg" alt="JavaScript" className="h-8" />
+        <div className="flex flex-wrap">
+          <img src="/Dart-Light.svg" alt="Dart" className="h-8 mr-2 mb-2" />
+          <img
+            src="/Flutter-Light.svg"
+            alt="Flutter"
+            className="h-8 mr-2 mb-2"
+          />
         </div>
       </div>
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Version Control</h2>
         <div className="flex flex-wrap">
-          <img src="/git.svg" alt="Git" className="h-8" />
-          <img src="/github-dark.svg" alt="GitHub" className="h-8" />
+          <img src="/Git.svg" alt="Git" className="h-8 mr-2 mb-2" />
+          <img src="/Github-Dark.svg" alt="GitHub" className="h-8 mr-2 mb-2" />
         </div>
       </div>
     </div>
   )
 }
-
 export default FrontSkill
