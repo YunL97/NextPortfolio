d4bbb89647fc1329a4135a573384ddaafb1170b8
YunL97 <leeys9932@naver.com>
2024-06-27 23:07:17 +0800

    이미지 경로 변경

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
   swcMinify: true,
   // next 애플리ㅋ케이
-  reactStrictMode: true, 
+  reactStrictMode: true,
   // env 내용 추가
   env: {
     BASE_URL: process.env.NEXT_PUBLIC_URL
   },
-  ...(process.env.NEXT_PUBLIC_NODE_ENV === 'prod' && {
+  ...(process.env.NEXT_PUBLIC_NODE_ENV === "prod" && {
     compiler: {
       removeConsole: {
-        exclude: ['error', 'warn']
+        exclude: ["error", "warn"]
       }
     }
   })
--- src/app/components/main/Skill/detailSkill.tsx
+++ src/app/components/main/Skill/detailSkill.tsx
@@ -4,35 +4,35 @@ const FrontSkill = () => {
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Frontend</h2>
         <div className=" flex flex-wrap">
-          <img src="html.svg" alt="HTML5" className="h-8" />
-          <img src="css.svg" alt="CSS3" className="h-8" />
-          <img src="javascript.svg" alt="JavaScript" className="h-8" />
-          <img src="typescript.svg" alt="JavaScript" className="h-8" />
-          <img src="vue.svg" alt="JavaScript" className="h-8" />
-          <img src="react.svg" alt="JavaScript" className="h-8" />
-          <img src="nextjs-dark.svg" alt="JavaScript" className="h-8" />
+          <img src="/HTML.svg" alt="HTML5" className="h-8" />
+          <img src="/css.svg" alt="CSS3" className="h-8" />
+          <img src="/javascript.svg" alt="JavaScript" className="h-8" />
+          <img src="/typescript.svg" alt="JavaScript" className="h-8" />
+          <img src="/vue.svg" alt="JavaScript" className="h-8" />
+          <img src="/react.svg" alt="JavaScript" className="h-8" />
+          <img src="/nextjs-dark.svg" alt="JavaScript" className="h-8" />
         </div>
       </div>
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Backend</h2>
         <div className=" flex flex-wrap">
-          <img src="nodejs.svg" alt="Python" className="h-8" />
-          <img src="python-dark.svg" alt="Python" className="h-8" />
-          <img src="fastapi.svg" alt="Django" className="h-8" />
+          <img src="/nodejs.svg" alt="Python" className="h-8" />
+          <img src="/python-dark.svg" alt="Python" className="h-8" />
+          <img src="/fastapi.svg" alt="Django" className="h-8" />
         </div>
       </div>
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Mobile App</h2>
         <div className=" flex flex-wrap">
-          <img src="dart-light.svg" alt="JavaScript" className="h-8" />
-          <img src="flutter-light.svg" alt="JavaScript" className="h-8" />
+          <img src="/dart-light.svg" alt="JavaScript" className="h-8" />
+          <img src="/flutter-light.svg" alt="JavaScript" className="h-8" />
         </div>
       </div>
       <div className="bg-white p-4 rounded-lg shadow-lg flex-shrink-0 mb-3">
         <h2 className="text-red-500 font-bold mb-2">Version Control</h2>
         <div className="flex flex-wrap">
-          <img src="git.svg" alt="Git" className="h-8" />
-          <img src="github-dark.svg" alt="GitHub" className="h-8" />
+          <img src="/git.svg" alt="Git" className="h-8" />
+          <img src="/github-dark.svg" alt="GitHub" className="h-8" />
         </div>
       </div>
     </div>
