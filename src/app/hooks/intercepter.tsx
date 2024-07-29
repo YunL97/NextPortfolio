9fd7c3e7a52c0f711cc4081a7dbd47b4b622980c
YunL97 <leeys9932@naver.com>
2024-07-29 22:21:08 +0900

    인터셉터 추가

---
+++ src/app/hooks/intercepter.tsx
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+// Axios 인스턴스 생성
+const api = axios.create({
+  baseURL: "https://yunl97.store"
+})
+
+// 요청 인터셉터 추가
+api.interceptors.request.use(
+  config => {
+    // 로컬 스토리지에서 토큰 가져오기
+    const token = localStorage.getItem("token")
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+export default api
