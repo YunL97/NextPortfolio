c2cfd8f8a27e24895a2f988fc5c7c38c72f88c3d
YunL97 <leeys9932@naver.com>
2024-08-06 11:20:48 +0900

    텍스트 수정

--- src/app/components/main/Name.tsx
+++ src/app/components/main/Name.tsx
@@ -2,7 +2,9 @@ import TestComponent from "@/app/(route)/LocalPodo/page"
 
 const Name = () => (
   <div className="justify-center w-full  font-bold text-blac items-end h-80 flex items-center">
-    <div className="font-sans text-3xl">이윤식 웹개발자 포트폴리오</div>
+    <div className="font-sans text-3xl">
+      이윤식 프론트엔드 개발자 포트폴리오
+    </div>
     {/* <TestComponent /> */}
   </div>
 )
